8f7d19c5f528f44f240d058e9c1cd5e62bb3b353
Вячеслав <74514540+morgunovvb@users.noreply.github.com>
2021-12-24 21:52:06 +0300

    Yatube v. 02.04

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
     path('', views.index),
@@ -9,4 +11,6 @@ urlpatterns = [
     path('groups/', views.groups),
     # Отдельная страница с информацией о посте
     path('group_posts/<slug:pk>/', views.group_posts),
+    # Четвертая страница
+    path('group_list.html/', views.group_list),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,28 @@
-# ice_cream/views.py
+# posts/views.py
 from django.http import HttpResponse
+# Импортируем загрузчик.
+from django.template import loader
+# Импортируем рендеринг
+from django.shortcuts import render
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
-
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
 
 # Страница со списком групп
 def groups(request):
-    return HttpResponse('Список групп')
-
+    template = 'posts/groups.html'
+    return render(request, template)
 
 # Страница с информацией о группе;
 # view-функция принимает параметр slug:slug из path()
 def group_posts(request, pk):
-    return HttpResponse(f'Группа номер {pk}')
+    template = 'posts/group_posts.html'
+    return render(request, template)
+
+# Страница четвертая
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
